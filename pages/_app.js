9735d0cfcd479c48e0b9cfbf621ed3191e141ae7
tderuvo <tonyderuvo@mac.com>
2022-10-22 14:03:28 -0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
