9ff904496a56f4df76e07d9d7567f11e3348fc73
tderuvo <tonyderuvo@mac.com>
2022-11-29 15:06:19 -0800

    a bunch of stuff

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd86ef9e1d0ce037"; }
+/******/ 		__webpack_require__.h = function() { return "f138877a4bc301b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.530ae54bffcab522.hot-update.js
+++ .next/static/webpack/webpack.530ae54bffcab522.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ecdcb5f7dea1f339"; }
+/******/ 	__webpack_require__.h = function() { return "f138877a4bc301b5"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
